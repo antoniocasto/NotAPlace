04ab87c6aa0fe2ef2743b26745c050ef5302bfdf
antoniocasto <mail.antoniocasto@gmail.com>
2023-03-09 14:57:11 +0100

    Initial Commit

---
+++ MemoroApp/MemoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoroAppApp.swift
+//  MemoroApp
+//
+//  Created by Antonio Casto on 09/03/23.
+//
+
+import SwiftUI
+
+@main
+struct MemoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
