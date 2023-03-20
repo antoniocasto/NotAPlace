1fcfc24c274ad328ed7bee49d38ea0b148aa010f
antoniocasto <mail.antoniocasto@gmail.com>
2023-03-20 18:57:05 +0100

    Add Map to World Map view.

---
+++ MemoroApp/Manager/LocationManager.swift
@@ -0,0 +1,66 @@
+//
+//  LocationManager.swift
+//  MemoroApp
+//
+//  Created by Antonio Casto on 20/03/23.
+//
+
+import Foundation
+import CoreLocation
+import MapKit
+
+enum MapDetails {
+    static let startingLocation =
+    CLLocationCoordinate2D(latitude: 37.331516, longitude: -121.891054)
+    static let defaultSpan = MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+}
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    
+    private var manager = CLLocationManager()
+        
+    @Published var showErrorAlert = false
+    @Published var showRestrictedSettingsAlert = false
+    
+    @Published var region = MKCoordinateRegion(center: MapDetails.startingLocation, span: MapDetails.defaultSpan)
+    
+    override init() {
+        super.init()
+        
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+
+    }
+    
+    private func checkLocationAuthorization() {
+                
+        switch manager.authorizationStatus {
+            
+        case .notDetermined:
+            manager.requestWhenInUseAuthorization()
+        case .restricted:
+            // Location is restricted i.e. due to parental controls.
+            showRestrictedSettingsAlert = true
+        case .denied:
+            // Location access denied
+            showErrorAlert = true
+        case .authorizedAlways, .authorizedWhenInUse:
+            guard let location = manager.location else { return }
+            region = MKCoordinateRegion(center: location.coordinate, span: MapDetails.defaultSpan)
+        @unknown default:
+            break
+        }
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error getting location: \(error)")
+        showErrorAlert = true
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+    
+    
+}
--- MemoroApp/View/WorldMapView.swift
+++ MemoroApp/View/WorldMapView.swift
@@ -6,15 +6,79 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct WorldMapView: View {
+    
+    @StateObject private var locationManager = LocationManager()
+ 
     var body: some View {
-        Text("World Map View")
+        
+        NavigationStack {
+            
+            ZStack {
+                
+                Map(coordinateRegion: $locationManager.region, interactionModes: .all, showsUserLocation: true)
+                    .tint(.accentColor)
+                
+                
+                pointer
+                
+            }
+            .edgesIgnoringSafeArea(.top)
+            
+            
+        }
+        // Alert for location access denied or error getting location 
+        .alert(WorldMapView.errorTitle, isPresented: $locationManager.showErrorAlert) {
+            Button(WorldMapView.cancelButton, role: .cancel) { }
+            Button(WorldMapView.openSettingsButtonText) {
+                // Get the App Settings URL in iOS Settings and open it.
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+        } message: {
+            Text(WorldMapView.errorMessage)
+        }
+        // Alert for location access restricted
+        .alert(WorldMapView.errorTitle, isPresented: $locationManager.showRestrictedSettingsAlert) {
+            Button(WorldMapView.cancelButton, role: .cancel) { }
+            Button(WorldMapView.openSettingsButtonText) {
+                // Get the App Settings URL in iOS Settings and open it.
+                if let url = URL(string: UIApplication.openSettingsURLString) {
+                    UIApplication.shared.open(url)
+                }
+            }
+        } message: {
+            Text(WorldMapView.errorRestrictionMessage)
+        }
+        
     }
+    
+    var pointer: some View {
+        Circle()
+            .fill(Color.accentColor.opacity(0.3))
+            .frame(width: 32, height: 32)
+            .overlay {
+                Circle()
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.white.opacity(0.7))
+            }
+    }
+    
 }
 
 struct WorldMapView_Previews: PreviewProvider {
     static var previews: some View {
         WorldMapView()
     }
+}
+
+extension WorldMapView {
+    static let errorTitle = LocalizedStringKey("WorldMapView.ErrorTitle")
+    static let cancelButton = LocalizedStringKey("WorldMapView.Cancel")
+    static let openSettingsButtonText = LocalizedStringKey("WorldMapView.Open Settings")
+    static let errorMessage = LocalizedStringKey("WorldMapView.Error Message")
+    static let errorRestrictionMessage = LocalizedStringKey("WorldMapView.Error Restriction Message")
 }
