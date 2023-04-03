f8e1b8b1be7ce8a442449c92c91d30561fd91369
antoniocasto <mail.antoniocasto@gmail.com>
2023-04-03 10:53:31 +0200

    Add slideshow view placeholder

--- MemoroApp/View/PlacesSlideshowView.swift
+++ MemoroApp/View/PlacesSlideshowView.swift
@@ -12,45 +12,25 @@ struct PlacesSlideshowView: View {
     
     @EnvironmentObject var placeManager: PlaceManager
     
+    @AppStorage("SelectedTab") var selectedTab: TabNavigationView.TabSelectable = .places
+    
     @State private var showPlaceDetail = false
     @State private var selectedPlace: Location?
-        
+    
     var body: some View {
         
         NavigationStack {
-            GeometryReader { proxy in
+            
+            Group {
                 
-                let width = proxy.size.width - (proxy.size.width * 10 / 100)
-                let height = proxy.size.height  - (proxy.size.height * 10 / 100)
-                
-                ScrollView(.horizontal, showsIndicators: false) {
-                    LazyHStack {
-                        ForEach(placeManager.places) { place in
-                            Group {
-                                
-                                if let imageName = place.image {
-                                    
-                                    let image = placeManager.loadImage(imageName: imageName)
-                                    
-                                    
-                                    ImageSlideshowCard(width: width, height: height, place: place, image: image)
-                                        .frame(width: width, height: height)
-                                    
-                                } else {
-                                    MapSlideshowCard(width: width, height: height, place: place, coordinate: place.coordinate)
-                                        .frame(width: width, height: height)
-                                }
-                            }
-                            .onTapGesture {
-                                selectedPlace = place
-                                showPlaceDetail.toggle()
-                            }
-
-                        }
-                    }
-                    .frame(maxHeight: .infinity)
-                    .padding(.horizontal)
-                    .ignoresSafeArea()
+                if placeManager.places.count != 0 {
+                    
+                    slideshow
+                    
+                } else {
+                    
+                    noPlacesPlaceholder
+                    
                 }
                 
             }
@@ -61,9 +41,68 @@ struct PlacesSlideshowView: View {
             }
             .navigationTitle(PlacesSlideshowView.navigationTitleText)
             .navigationBarTitleDisplayMode(.large)
+            
         }
         
     }
+    
+    var slideshow: some View {
+        GeometryReader { proxy in
+            
+            
+            let width = proxy.size.width - (proxy.size.width * 10 / 100)
+            let height = proxy.size.height  - (proxy.size.height * 10 / 100)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                LazyHStack {
+                    ForEach(placeManager.places) { place in
+                        Group {
+                            
+                            if let imageName = place.image {
+                                
+                                let image = placeManager.loadImage(imageName: imageName)
+                                
+                                
+                                ImageSlideshowCard(width: width, height: height, place: place, image: image)
+                                    .frame(width: width, height: height)
+                                
+                            } else {
+                                MapSlideshowCard(width: width, height: height, place: place, coordinate: place.coordinate)
+                                    .frame(width: width, height: height)
+                            }
+                        }
+                        .onTapGesture {
+                            selectedPlace = place
+                            showPlaceDetail.toggle()
+                        }
+                        
+                    }
+                }
+                .frame(maxHeight: .infinity)
+                .padding(.horizontal)
+                .ignoresSafeArea()
+            }
+        }
+    }
+    
+    var noPlacesPlaceholder: some View {
+        VStack(alignment: .center) {
+            Image(systemName: "mappin.slash.circle.fill")
+                .font(.system(size: 60))
+                .foregroundColor(.secondary)
+            Text(PlacesSlideshowView.noPlacesText)
+                .font(.title2.bold())
+                .padding(8)
+            
+            Button(PlacesSlideshowView.startText) {
+                selectedTab = .map
+            }
+            .bold()
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
 }
 
 struct PlacesSlideshowView_Previews: PreviewProvider {
@@ -75,4 +114,6 @@ struct PlacesSlideshowView_Previews: PreviewProvider {
 
 extension PlacesSlideshowView {
     static let navigationTitleText = LocalizedStringKey("PlacesSlideshowView.navigationTitleText")
+    static let noPlacesText = LocalizedStringKey("PlacesSlideshowView.noPlacesText")
+    static let startText = LocalizedStringKey("PlacesSlideshowView.startText")
 }
